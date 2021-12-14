000f5cfb2f6c6028eaeabde26da596729952407b
Rodrigo Ribeiro Dias <rrd.rodrigodias@gmail.com>
2021-12-14 19:00:58 -0300

    Add circlici configs

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-rrd-env.eba-u966jn7v.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
